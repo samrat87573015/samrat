3102552d25dfbf9c2bbfd8d15c1f658a97f3d166
samrat87573015 <www.samrat87573015@gmail.com>
2024-07-31 16:54:23 +0600

    done

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,15 +1,42 @@
 
+//watch the free video on how this demo was made
+// https://www.snorkl.tv/scrolltrigger-smooth-scroll/
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-ScrollTrigger.normalizeScroll(true)
-
-// create the smooth scroller FIRST!
-let smoother = ScrollSmoother.create({
-    smooth: 2,
-    effects: true,
-    normalizeScroll: true
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".scrollContainer"),
+    smooth: true
 });
+
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+
+
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".scrollContainer", {
+    scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) :    locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".scrollContainer").style.transform ? "transform" : "fixed"
+});
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+//watch the free video on how this demo was made
+// https://www.snorkl.tv/scrolltrigger-smooth-scroll/
+
 
 
 
@@ -23,9 +50,10 @@ function heroAnimation() {
     scrollTrigger: {
       trigger: ".hero_area",
       start: "top top",
-      end: "+=300",
+      end: "+=600",
       scrub: 2,
         pin: true,
+        scroller:".scrollContainer",
     },
   });
 
@@ -87,6 +115,7 @@ function bodyColorChangeAnimation(){
             trigger: e,
             start: "top 20%",
             end: "bottom 80%",
+            scroller:".scrollContainer",
             onEnter: function(){
                 document.body.setAttribute("theme", e.dataset.color);
                 header.setAttribute("theme", e.dataset.color);
@@ -102,21 +131,41 @@ function bodyColorChangeAnimation(){
 bodyColorChangeAnimation();
 
 function scollSliderAnimation() {
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-    gsap.to(".slider_item", {
-        scrollTrigger: {
-            trigger: ".scrol_slider_area",
-            start: "top top",
-            end: "+=600",
-            scrub: 2,
-            pin: true,
-        },
-        xPercent: -200,
-        ease: Power4,
-        duration: 3,
-    });
+    if(isMobile){
+        gsap.to(".slider_item", {
+            scrollTrigger: {
+                trigger: ".scrol_slider_area",
+                start: "top 50%",
+                end: "+=300",
+                scrub: 2,
+                pin: true,
+                scroller:".scrollContainer",
+            },
+            xPercent: -200,
+            ease: Power4,
+            duration: 3,
+        });
+    }else {
+        gsap.to(".slider_item", {
+            scrollTrigger: {
+                trigger: ".scrol_slider_area",
+                start: "top -90%",
+                end: "+=1200",
+                scrub: 2,
+                pin: true,
+                scroller:".scrollContainer",
+            },
+            xPercent: -200,
+            ease: Power4,
+            duration: 3,
+        });
+    }
+
 }
 scollSliderAnimation();
+window.addEventListener("resize", scollSliderAnimation);
 
 function aboutAnimation(){
     const isMobile = window.matchMedia("(max-width: 768px)").matches;
@@ -128,6 +177,7 @@ function aboutAnimation(){
                 start: "top 30%",
                 end: "bottom bottom",
                 scrub: 3,
+                scroller:".scrollContainer",
 
             },
             y: -300,
@@ -140,15 +190,15 @@ function aboutAnimation(){
             scrollTrigger: {
                 trigger: '.about',
                 start: "top 15%",
-                end: "+=1000",
+                end: "+=600",
                 pin: true,
                 scrub: 3,
-                duration: 3,
+                scroller:".scrollContainer",
 
             },
             y: -1345,
             ease: Power4,
-            duration: 2,
+            duration: 300,
             stagger: 0.03,
         })
     }
@@ -186,9 +236,10 @@ gsap.to('.project_item', {
     scrollTrigger: {
         trigger: '.project_area',
         start: "top 5%",
-        end: "+=600",
+        end: "+=1000",
         scrub: 3,
         pin: true,
+        scroller:".scrollContainer",
     },
     y: 0,
     ease: Power4,
